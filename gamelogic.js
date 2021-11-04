fa8b5badcf91bee4f6833a1e3f14834e1c6cfbfa
Dan-DH <daniel.diaz.hdez@gmail.com>
2021-11-04 23:10:29 +0100

    first commit

---
+++ gamelogic.js
@@ -0,0 +1,14 @@
+import { Person } from "./character.js"
+
+
+var p1 = new Person("human", "staff")
+
+function classes(p, c) {
+    if ( c === "assasin") {
+        player.doge = 10;
+    } else if (c === "berserker") {
+       p
+    }
+    }
+} 
+console.log(testo.race)
